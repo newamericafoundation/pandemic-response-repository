898b99e25475ea2043701418077182bfe8fe60ba
Mark Lerner <markjonathanlerner@gmail.com>
2020-07-10 14:37:54 -0400

    Remove leading and trailing hyphens from URLs.

--- _plugins/data_page_generator.rb
+++ _plugins/data_page_generator.rb
@@ -14,7 +14,7 @@ module Jekyll
       return name.tr(
         "ÀÁÂÃÄÅàáâãäåĀāĂăĄąÇçĆćĈĉĊċČčÐðĎďĐđÈÉÊËèéêëĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħÌÍÎÏìíîïĨĩĪīĬĭĮįİıĴĵĶķĸĹĺĻļĽľĿŀŁłÑñŃńŅņŇňŉŊŋÑñÒÓÔÕÖØòóôõöøŌōŎŏŐőŔŕŖŗŘřŚśŜŝŞşŠšſŢţŤťŦŧÙÚÛÜùúûüŨũŪūŬŭŮůŰűŲųŴŵÝýÿŶŷŸŹźŻżŽž",
         "AAAAAAaaaaaaAaAaAaCcCcCcCcCcDdDdDdEEEEeeeeEeEeEeEeEeGgGgGgGgHhHhIIIIiiiiIiIiIiIiIiJjKkkLlLlLlLlLlNnNnNnNnnNnNnOOOOOOooooooOoOoOoRrRrRrSsSsSsSssTtTtTtUUUUuuuuUuUuUuUuUuUuWwYyyYyYZzZzZz"
-      ).downcase.strip.gsub(/[^[:alnum:]]+/, '-').gsub(/[^\w.-]/, '')
+      ).downcase.strip.gsub(/[^[:alnum:]]+/, '-').gsub(%r!^-|-$!i, "").gsub(/[^\w.-]/, '')
     end
   end
 
